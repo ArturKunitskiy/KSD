f597005075e95f070998b69447d733506b356759
Oleksandra <alexa30che@gmail.com>
2025-04-18 11:33:48 +0300

    add navigation, categories

--- KSD/src/App.js
+++ KSD/src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {MainPage} from './components/main'
-import {ScrollingText} from './components/main'
+import { MainPage, Home } from './components/main'
+import { ScrollingText } from './components/main'
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        {/* <Route index element={} /> */}
-        {/* <Route path="/ProgrammingBooks" element={<></>} /> */}
-        {/* <Route path="/Hobbit" element={<></>} /> */}
+        <Route path="/" element={<MainPage />}>
+          <Route index element={<Home></Home>} />
+          {/* <Route path="/ProgrammingBooks" element={<></>} /> */}
+          {/* <Route path="/Hobbit" element={<></>} /> */}
+        </Route>
+
         {/* <Route path="/Cart" element={<></>} /> */}
         {/* <Route path="/Profile/MyProfile" element={<></>} /> */}
         {/* <Route path="/Profile/Orders" element={<></>} /> */}
--- KSD/src/components/main.js
+++ KSD/src/components/main.js
@@ -2,22 +2,11 @@ import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 export function ScrollingText() {
+    const discountArray = ['Безкоштовно* від 790 грн', 'Безкоштовно* від 790 грн', 'Безкоштовно* від 790 грн', 'Безкоштовно* від 790 грн', 'Безкоштовно* від 790 грн', 'Безкоштовно* від 790 грн', 'Безкоштовно* від 790 грн', 'Безкоштовно* від 790 грн', 'Безкоштовно* від 790 грн', 'Безкоштовно* від 790 грн', 'Безкоштовно* від 790 грн', 'Безкоштовно* від 790 грн', 'Безкоштовно* від 790 грн']
     return <div className='scrolling'>
         <div className='scroll'>
             <div className='leftToRight'>
-                <p>Безкоштовно* від 790 грн</p>
-                <p>Безкоштовно* від 790 грн</p>
-                <p>Безкоштовно* від 790 грн</p>
-                <p>Безкоштовно* від 790 грн</p>
-                <p>Безкоштовно* від 790 грн</p>
-                <p>Безкоштовно* від 790 грн</p>
-                <p>Безкоштовно* від 790 грн</p>
-                <p>Безкоштовно* від 790 грн</p>
-                <p>Безкоштовно* від 790 грн</p>
-                <p>Безкоштовно* від 790 грн</p>
-                <p>Безкоштовно* від 790 грн</p>
-                <p>Безкоштовно* від 790 грн</p>
-                <p>Безкоштовно* від 790 грн</p>
+                {discountArray.map((item) => <p>{item}</p>)}
             </div>
         </div>
     </div>
@@ -25,23 +14,102 @@ export function ScrollingText() {
 
 export function Header() {
     return <header>
-        <Link to='/' style={{width: '245px'}}><img src='logo.png' alt='logo' /></Link>
+        <Link to='/' style={{ width: '245px' }}><img src='logo.png' alt='logo' /></Link>
         <div className='search'>
-            <input type='text' placeholder='Пошук товарів, категорій' />
-            <button style={{width: '48px', height: '48px', backgroundColor: '#B53535', borderRadius: '50%', border: 'none', cursor: 'pointer'}}><img src='search.png' alt='search icon' style={{width: '18px', height: '18px'}}/></button>
+            <input type='text' placeholder='Пошук товарів, категорій' style={{ lineHeight: '24px' }} />
+            <button style={{ width: '48px', height: '48px', backgroundColor: '#B53535', borderRadius: '50%', border: 'none', cursor: 'pointer', display: 'flex', justifyContent: 'center', alignItems: 'center' }}><img src='search.png' alt='search icon' style={{ width: '18px', height: '18px' }} /></button>
         </div>
         <div>
-            <Link to='/Profile/MyProfile' style={{marginRight: '14px', display: 'inline-block', width: '30px', height: '30px'}}><img src='user.png' alt='user icon'/></Link>
-            <Link to='/Cart' style={{display: 'inline-block', width: '30px', height: '30px'}}><img src='cart.png' alt='cart icon' /></Link>
+            <Link to='/Profile/MyProfile' style={{ marginRight: '14px', display: 'inline-block', width: '30px', height: '30px' }}><img src='user.png' alt='user icon' /></Link>
+            <Link to='/Cart' style={{ display: 'inline-block', width: '30px', height: '30px' }}><img src='cart.png' alt='cart icon' /></Link>
         </div>
 
     </header>
+}
+
+export function Category(props) {
+    return <Link to={props.link} style={{ height: '60px', backgroundColor: '#D6D9DC', color: '#3C3C3B', padding: '10px 40px', borderRadius: '30px', paddingTop: '12px' }} className='categoryText'>
+        {props.name}
+    </Link>
+}
+
+function NavigationSection(props) {
+    return <div style={{ marginTop: props.top }}><Link to='#' className='highlightedNav'>{props.highlighted}</Link>
+        {props.links.map((item, index) => <Link key={index} to={item.link} className="navA">{item.name}</Link>)}
+    </div>
+}
+
+export function Navigation() {
+    const section1 = [{ name: 'Романтична проза', link: '#' },
+    { name: 'Історична та пригодницька проза', link: '#' },
+    { name: 'Детективи', link: '#' },
+    { name: 'Трилери та жахи', link: '#' },
+    { name: 'Фантастика', link: '#' },
+    { name: 'Фентезі', link: '#' },
+    { name: 'Класична література', link: '#' },
+    { name: 'Комікси та манґи', link: '#' }]
+    const section2 = [{ name: 'Історія, факти та біографії', link: '#' },
+    { name: 'Психологія', link: '#' },
+    { name: 'Саморозвиток, мотивація', link: '#' },
+    { name: 'Бізнес-література', link: '#' },
+    { name: 'Дозвілля та Хобі', link: '#' },
+    { name: 'Наукпоп', link: '#' },
+    { name: 'Езотерика, таро', link: '#' }]
+    const section3 = [{ name: 'Дітям до 4-х років', link: '#' },
+    { name: 'Дітям 4-6 років', link: '#' },
+    { name: 'Дітям 7-12 років', link: '#' },
+    { name: 'Дітям від 12 років', link: '#' },
+    { name: 'Шкільна та Навчальна', link: '#' },
+    { name: 'Розвиваючі книги', link: '#' },
+    { name: 'Казки і повісті', link: '#' }]
+    const section4 = [{ name: 'Ексклюзивно у КСД', link: '#' },
+    { name: 'Книжки місяця', link: '#' },
+    { name: 'Новинки КСД', link: '#' },
+    { name: 'Анонси КСД', link: '#' },
+    { name: 'Новинки партнерів', link: '#' },
+    { name: 'Хіти продажу', link: '#' },
+    { name: 'Закладки для книжок', link: '#' },
+    { name: 'Розпродаж', link: '#' },
+    { name: 'Останні примірники', link: '#' },
+    { name: 'Тимчасово немає у продажу', link: '#' }]
+    return <div>
+        <nav>
+            <Link to='#' className='highlightedNav'>Художні</Link>
+            <NavigationSection highlighted='Сучасні автори' links={section1} top='10px'></NavigationSection>
+            <NavigationSection highlighted='Прикладні' links={section2} top='17px'></NavigationSection>
+            <NavigationSection highlighted='Дитячі' links={section3} top='17px'></NavigationSection>
+            <NavigationSection highlighted='Спецпропозиції' links={section4} top='17px'></NavigationSection>
+        </nav>
+        <p className='title' style={{ paddingLeft: '14px', marginTop: '32px' }}>Книгарні КСД</p>
+        <img src='shopsMap.png' style={{ width: '192px', marginTop: '32px', marginLeft: '14px' }}></img>
+    </div>
+}
+
+export function Home() {
+    return <main>
+
+    </main>
 }
 
 export function MainPage() {
     return <>
         <ScrollingText />
         <Header></Header>
-        <Outlet />
+        <div className='wrapper'>
+            <Category link='#' name='Художні'></Category>
+            <Category link='#' name='Прикладні'></Category>
+            <Category link='#' name='Дитячі'></Category>
+            <Category link='#' name='Спецпропозиції'></Category>
+            <Category link='#' name='eBooks'></Category>
+            <Category link='#' name='Анонси КСД'></Category>
+        </div>
+        <div className='wrapper'>
+            <Navigation></Navigation>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+
+
     </>
 }
